847004ce5cc9fe63ba158629285939eb5b9a3729
Jong jae Park <pjj186@naver.com>
2022-03-18 19:23:07 +0900

    모달창 틀 완성
    모달창 틀 완성

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,3 +1,13 @@
 import React from "react";
 
-const Modal = () => {};
+const Modal = ({ cancelModal }) => {
+  return (
+    <div className="w-full h-screen p-20 bg-teal-700 opacity-50 absolute">
+      <div className=" bg-cyan-100 w-full h-full">
+        <button onClick={cancelModal}>나는 모달 끄기</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Modal from "../components/Modal";
 
 const Home = () => {
+  const [modal, setModal] = useState(false);
+
+  const openModal = () => {
+    if (modal === false) {
+      setModal(true);
+    }
+  };
+
+  const cancelModal = () => {
+    if (modal === true) {
+      setModal(false);
+    }
+  };
+
   return (
-    <div className="w-full h-full p-6 space-y-3">
-      <div className="flex flex-col items-center w-10 cursor-pointer">
-        <div className=" w-16 h-16 bg-slate-500 rounded-lg mb-2" />
-        <span className=" text-white">한식</span>
+    <>
+      {modal ? <Modal cancelModal={cancelModal} /> : null}
+      <div className="w-full h-full p-6 space-y-3">
+        <div
+          className="flex flex-col items-center w-10 cursor-pointer"
+          onClick={openModal}
+        >
+          <div className=" w-16 h-16 bg-slate-500 rounded-lg mb-2" />
+          <span className=" text-white">한식</span>
+        </div>
+        <div className="flex flex-col items-center w-10 cursor-pointer">
+          <div className=" w-16 h-16 bg-slate-500 rounded-lg mb-2" />
+          <span className="text-white">중식</span>
+        </div>
+        <div className="flex flex-col items-center w-10 cursor-pointer">
+          <div className=" w-16 h-16 bg-slate-500 rounded-lg mb-2" />
+          <span className="text-white">일식</span>
+        </div>
+        <div className="flex flex-col items-center w-10 cursor-pointer">
+          <div className=" w-16 h-16 bg-slate-500 rounded-lg mb-2" />
+          <span className="text-white">양식</span>
+        </div>
       </div>
-      <div className="flex flex-col items-center w-10 cursor-pointer">
-        <div className=" w-16 h-16 bg-slate-500 rounded-lg mb-2" />
-        <span className="text-white">중식</span>
-      </div>
-      <div className="flex flex-col items-center w-10 cursor-pointer">
-        <div className=" w-16 h-16 bg-slate-500 rounded-lg mb-2" />
-        <span className="text-white">일식</span>
-      </div>
-      <div className="flex flex-col items-center w-10 cursor-pointer">
-        <div className=" w-16 h-16 bg-slate-500 rounded-lg mb-2" />
-        <span className="text-white">양식</span>
-      </div>
-    </div>
+    </>
   );
 };
 
