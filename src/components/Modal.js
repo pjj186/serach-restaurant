f7636e1267e163591d0a7f3470c608a57f84f33b
jongjaepark <pjj186@naver.com>
2023-10-20 14:04:18 +0900

    버튼 UI 이상한 현상 해결

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AppContext } from "../App";
-import styled from "styled-components";
-import MiniHeader from "./MiniHeader";
-import Location from "./location";
-import ResultLocation from "./ResultLocation";
+import React, { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../App';
+import styled from 'styled-components';
+import MiniHeader from './MiniHeader';
+import Location from './location';
+import ResultLocation from './ResultLocation';
 
 const ModalContainer = styled.div`
   background-color: rgba(15, 118, 110, 0.4);
@@ -28,23 +28,23 @@ const Modal = ({ cancelModal, category }) => {
   useEffect(() => {}, [isResult]);
 
   return (
-    <ModalContainer className="w-full h-screen p-20 absolute -top-5 flex items-center justify-center">
-      <div className=" bg-neutral-200 w-1/2 h-full rounded-lg">
+    <ModalContainer className='w-full h-screen p-20 absolute -top-5 flex items-center justify-center'>
+      <div className=' bg-neutral-200 w-1/2 h-full rounded-lg'>
         <MiniHeader cancelModal={cancelModal} category={category} />
         {!isResult ? (
           <div>
             <div>
-              <p className=" text-3xl mt-3 mb-3 text-center">
+              <p className=' text-3xl mt-3 mb-3 text-center'>
                 주변에 이런 식당들이 있어요!
               </p>
-              <div className="flex justify-center items-center pb-10 border-b-2 border-dashed">
+              <div className='flex justify-center items-center pb-10 border-b-2 border-dashed'>
                 <Location category={category} />
               </div>
             </div>
             {!locContext.loading ? (
-              <div className="flex items-center justify-center">
+              <div className='flex items-center justify-center'>
                 <span
-                  className="text-3xl border-2 border-solid mt-24 px-4 py-2 rounded-lg cursor-pointer shadow-lg hover:bg-slate-300"
+                  className='text-3xl border-2 border-solid mt-12 px-4 py-2 rounded-lg cursor-pointer shadow-lg hover:bg-slate-300'
                   onClick={pickRes}
                 >
                   식당 뽑기!
@@ -55,25 +55,25 @@ const Modal = ({ cancelModal, category }) => {
         ) : (
           <div>
             <div>
-              <p className=" text-3xl mt-3 mb-3 text-center">여긴 어때요?</p>
-              <div className="flex flex-col justify-center items-center pb-10 border-b-2 border-dashed">
+              <p className=' text-3xl mt-3 mb-3 text-center'>여긴 어때요?</p>
+              <div className='flex flex-col justify-center items-center pb-10 border-b-2 border-dashed'>
                 {res === undefined ? (
-                  <div className="w-[500px] h-[408px] flex items-center justify-center">
+                  <div className='w-[500px] h-[408px] flex items-center justify-center'>
                     <span>검색된 식당이 없어요.. 다른 식당을 찾아봐요!</span>
                   </div>
                 ) : (
                   <>
                     <ResultLocation res={res.road_address_name} />
-                    <span className="mt-4 font-bold text-3xl text-gray-900">
+                    <span className='mt-4 font-bold text-3xl text-gray-900'>
                       {res.place_name}
                     </span>
                   </>
                 )}
               </div>
             </div>
-            <div className="flex items-center justify-center">
+            <div className='flex items-center justify-center'>
               <span
-                className="text-3xl border-2 border-solid mt-24 px-4 py-2 rounded-lg cursor-pointer shadow-lg hover:bg-slate-300"
+                className='text-3xl border-2 border-solid mt-6 px-4 py-2 rounded-lg cursor-pointer shadow-lg hover:bg-slate-300'
                 onClick={resetBtn}
               >
                 다시 뽑기!
